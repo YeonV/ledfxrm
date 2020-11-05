0cd567262c560c80f3b38275c09f701d2ff28d01
YeonV <trusted.yeon@gmail.com>
2020-11-05 02:44:10 +0100

    Implement Blueprint - Remove functionality

---
+++ custom_components/ledfxrm/config_flow.py
@@ -0,0 +1,107 @@
+"""Adds config flow for Ledfxrm."""
+from homeassistant import config_entries
+from homeassistant.core import callback
+import voluptuous as vol
+import requests
+from custom_components.blueprint.const import (  # pylint: disable=unused-import
+    CONF_HOST,
+    CONF_PORT,
+    DOMAIN,
+    PLATFORMS,
+)
+
+
+class LedfxrmFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
+    """Config flow for Ledfxrm."""
+
+    VERSION = 1
+    CONNECTION_CLASS = config_entries.CONN_CLASS_CLOUD_POLL
+
+    def __init__(self):
+        """Initialize."""
+        self._errors = {}
+
+    async def async_step_user(
+        self, user_input=None  # pylint: disable=bad-continuation
+    ):
+        """Handle a flow initialized by the user."""
+        self._errors = {}
+
+        # Uncomment the next 2 lines if only a single instance of the integration is allowed:
+        if self._async_current_entries():
+            return self.async_abort(reason="single_instance_allowed")
+
+        if user_input is not None:
+            valid = await self._test_credentials(
+                user_input[CONF_HOST], user_input[CONF_PORT]
+            )
+            if valid:
+                return self.async_create_entry(
+                    title=user_input[CONF_HOST], data=user_input
+                )
+            else:
+                self._errors["base"] = "auth"
+
+            return await self._show_config_form(user_input)
+
+        return await self._show_config_form(user_input)
+
+    @staticmethod
+    @callback
+    def async_get_options_flow(config_entry):
+        return LedfxrmOptionsFlowHandler(config_entry)
+
+    async def _show_config_form(self, user_input):  # pylint: disable=unused-argument
+        """Show the configuration form to edit location data."""
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema(
+                {vol.Required(CONF_HOST): str, vol.Required(CONF_PORT): int}
+            ),
+            errors=self._errors,
+        )
+
+    async def _test_credentials(self, thehost, theport):
+        """Return true if credentials is valid."""
+        try:
+            client = requests.get("http://" + thehost + ":" + theport + "/api/info")
+            await client.async_get_data()
+            return True
+        except Exception:  # pylint: disable=broad-except
+            pass
+        return False
+
+
+class LedfxrmOptionsFlowHandler(config_entries.OptionsFlow):
+    """Ledfxrm config flow options handler."""
+
+    def __init__(self, config_entry):
+        """Initialize HACS options flow."""
+        self.config_entry = config_entry
+        self.options = dict(config_entry.options)
+
+    async def async_step_init(self, user_input=None):  # pylint: disable=unused-argument
+        """Manage the options."""
+        return await self.async_step_user()
+
+    async def async_step_user(self, user_input=None):
+        """Handle a flow initialized by the user."""
+        if user_input is not None:
+            self.options.update(user_input)
+            return await self._update_options()
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema(
+                {
+                    vol.Required(x, default=self.options.get(x, True)): bool
+                    for x in sorted(PLATFORMS)
+                }
+            ),
+        )
+
+    async def _update_options(self):
+        """Update config entry options."""
+        return self.async_create_entry(
+            title=self.config_entry.data.get(CONF_HOST), data=self.options
+        )
---
+++ custom_components/ledfxrm/switch.py
@@ -0,0 +1,40 @@
+"""Switch platform for ledfxrm."""
+from homeassistant.components.switch import SwitchDevice
+
+from custom_components.ledfxrm.const import DEFAULT_NAME, DOMAIN, ICON_POWER, SWITCH
+from custom_components.ledfxrm.entity import LedfxrmEntity
+
+
+async def async_setup_entry(hass, entry, async_add_devices):
+    """Setup sensor platform."""
+    coordinator = hass.data[DOMAIN][entry.entry_id]
+    async_add_devices([LedfxrmBinarySwitch(coordinator, entry)])
+
+
+class LedfxrmBinarySwitch(LedfxrmEntity, SwitchDevice):
+    """ledfxrm switch class."""
+
+    async def async_turn_on(self, **kwargs):  # pylint: disable=unused-argument
+        """Turn on the switch."""
+        await self.coordinator.api.async_change_something(True)
+        await self.coordinator.async_request_refresh()
+
+    async def async_turn_off(self, **kwargs):  # pylint: disable=unused-argument
+        """Turn off the switch."""
+        await self.coordinator.api.async_change_something(False)
+        await self.coordinator.async_request_refresh()
+
+    @property
+    def name(self):
+        """Return the name of the switch."""
+        return f"{DEFAULT_NAME}_{SWITCH}"
+
+    @property
+    def icon(self):
+        """Return the icon of this switch."""
+        return ICON_POWER
+
+    @property
+    def is_on(self):
+        """Return true if the switch is on."""
+        return self.coordinator.api.something
