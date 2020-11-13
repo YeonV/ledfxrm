cdc8511c1ac0a3a68bcdfe2b7b2965b94cb50001
YeonV <trusted.yeon@gmail.com>
2020-11-13 16:35:26 +0100

    v0.1.1

--- custom_components/ledfxrm/const.py
+++ custom_components/ledfxrm/const.py
@@ -3,7 +3,7 @@
 NAME = "LedFX ReMote"
 DOMAIN = "ledfxrm"
 DOMAIN_DATA = f"{DOMAIN}_data"
-VERSION = "0.1.0"
+VERSION = "0.1.1"
 MANUFACTURER = "YeonV"
 
 ISSUE_URL = "https://github.com/YeonV/ledfxrm/issues"
@@ -33,6 +33,7 @@ CONF_HOST = "host"
 CONF_PORT = "port"
 CONF_START = "start"
 CONF_STOP = "stop"
+CONF_ADVANCED = "advanced"
 
 # Defaults
 DEFAULT_NAME = DOMAIN
